0cfb30e530fcc7e184a7d3b0915286044bcf5973
Arunabh Das <arunabhdas@gmail.com>
2025-03-05 06:03:14 -0500

    Added firebaseAuth dependency to version catalog

--- saaslaunchpad-compose-multiplatform/composeApp/build.gradle.kts
+++ saaslaunchpad-compose-multiplatform/composeApp/build.gradle.kts
@@ -89,6 +89,7 @@ kotlin {
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
+            implementation(libs.firebase.auth)
 
         }
 
