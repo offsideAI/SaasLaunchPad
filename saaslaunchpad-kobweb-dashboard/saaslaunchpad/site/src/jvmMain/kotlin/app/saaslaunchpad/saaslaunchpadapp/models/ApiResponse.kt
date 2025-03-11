1c78d158c8bdc3f8e19b66d5f057e2ca4f024701
Arunabh Das <arunabhdas@gmail.com>
2025-03-11 18:22:43 -0400

    Added sealed class for ApiResponse

--- saaslaunchpad-kobweb-dashboard/saaslaunchpad/site/src/jvmMain/kotlin/app/saaslaunchpad/saaslaunchpadapp/models/ApiResponse.kt
+++ saaslaunchpad-kobweb-dashboard/saaslaunchpad/site/src/jvmMain/kotlin/app/saaslaunchpad/saaslaunchpadapp/models/ApiResponse.kt
@@ -1,7 +1,14 @@
 package app.saaslaunchpad.saaslaunchpadapp.models
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 sealed class ApiResponse {
+    @Serializable
+    @SerialName("success")
+    data class Success(val data: List<Person>): ApiResponse()
+    @Serializable
+    @SerialName("error")
+    data class Error(val errorMessage: String): ApiResponse()
 }
