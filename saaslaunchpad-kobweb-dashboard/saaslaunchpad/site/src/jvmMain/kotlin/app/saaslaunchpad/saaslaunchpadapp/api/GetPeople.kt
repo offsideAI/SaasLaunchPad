97ca2143fd1fbe9d5c2f55c681226b89f3e140ce
Arunabh Das <arunabhdas@gmail.com>
2025-03-12 06:21:00 -0400

    Implementation of getPeople API endpoint

---
+++ saaslaunchpad-kobweb-dashboard/saaslaunchpad/site/src/jvmMain/kotlin/app/saaslaunchpad/saaslaunchpadapp/api/GetPeople.kt
@@ -0,0 +1,32 @@
+package app.saaslaunchpad.saaslaunchpadapp.api
+
+import app.saaslaunchpad.saaslaunchpadapp.models.ApiResponse
+import app.saaslaunchpad.saaslaunchpadapp.models.Person
+import com.varabyte.kobweb.api.Api
+import com.varabyte.kobweb.api.ApiContext
+import com.varabyte.kobweb.api.http.setBodyText
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+val people = listOf(
+    Person(name = "Alice", age = 31),
+    Person(name = "Bob", age = 32),
+    Person(name = "John", age = 33),
+    Person(name = "Jane", age = 34),
+)
+
+@Api
+suspend fun getPeople(context: ApiContext) {
+    try {
+        val number = context.req.params.getValue("count").toInt()
+        context.res.setBodyText(
+            Json.encodeToString<ApiResponse>(
+                ApiResponse.Success(data = people.take(number))
+            )
+        )
+    } catch (e: Exception) {
+        Json.encodeToString<ApiResponse>(
+           ApiResponse.Error(errorMessage = e.message.toString())
+        )
+    }
+}
