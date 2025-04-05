f82485c3387f384b0c6e85cb3c1324c2c0903127
Arunabh Das <arunabhdas@gmail.com>
2025-04-05 00:19:27 -0400

    Updated tsconfig.json and ran ==> npm run build

--- saaslaunchpad-mcp/dist/main.js
+++ saaslaunchpad-mcp/dist/main.js
@@ -1,1 +1,21 @@
-"use strict";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+const server = new McpServer({
+    name: "SaasLaunchPad Service",
+    version: "1.0.0",
+});
+server.tool('getWeather', {
+    city: z.string()
+}, async ({ city }) => {
+    return {
+        content: [
+            {
+                type: "text",
+                text: `The weather in ${city} is cloudy!`,
+            },
+        ],
+    };
+});
+const transport = new StdioServerTransport();
+await server.connect(transport);
---
+++ saaslaunchpad-mcp/main.ts
@@ -0,0 +1,1 @@
+import { McpServer } from @model
