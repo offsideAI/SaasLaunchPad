d7dcc7a8122dc730a56fb989223c172c5674d60d
Arunabh Das <arunabhdas@gmail.com>
2025-04-15 05:49:12 -0400

    Added saaslaunchpadapp-ktor

---
+++ saaslaunchpadapp-ktor/src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package app.saaslaunchpad
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
