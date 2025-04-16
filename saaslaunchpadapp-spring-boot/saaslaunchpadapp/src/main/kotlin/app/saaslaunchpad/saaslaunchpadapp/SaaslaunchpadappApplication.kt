32cd920182d27a5a53b02fe20080a29b74ef03c0
Arunabh Das <arunabhdas@gmail.com>
2025-04-16 02:31:49 -0400

    Scaffolded SaasLaunchPadApp using start.spring.io

---
+++ saaslaunchpadapp-spring-boot/saaslaunchpadapp/src/main/kotlin/app/saaslaunchpad/saaslaunchpadapp/SaaslaunchpadappApplication.kt
@@ -0,0 +1,11 @@
+package app.saaslaunchpad.saaslaunchpadapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SaaslaunchpadappApplication
+
+fun main(args: Array<String>) {
+	runApplication<SaaslaunchpadappApplication>(*args)
+}
---
+++ saaslaunchpadapp-spring-boot/saaslaunchpadapp/src/test/kotlin/app/saaslaunchpad/saaslaunchpadapp/SaaslaunchpadappApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.saaslaunchpad.saaslaunchpadapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SaaslaunchpadappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
