dc57663362845aa6ae20ee39e8264fc8d3f4733e
Arunabh Das <arunabhdas@gmail.com>
2025-04-16 06:32:48 -0400

    Added list of cards

--- saaslaunchpadapp-ktor/saaslaunchpadapp_ktor/src/main/kotlin/routes/CardRoute.kt
+++ saaslaunchpadapp-ktor/saaslaunchpadapp_ktor/src/main/kotlin/routes/CardRoute.kt
@@ -1,7 +1,16 @@
 package app.saaslaunchpad.routes
 
 
-private const val BASE_URL = "http://localhost:8090"
+private const val BASE_URL = "http://192.168.86.182:8090"
+
+private val cards = listOf(
+    Card("Benjamin", "Easy to teach, paws to beat", "$BASE_URL/dog1.png"),
+    Card("Holly", "A real fast learner!", "$BASE_URL/dog2.png"),
+    Card("Queen Noor", "Queen of all she surveys", "$BASE_URL/dog3.png"),
+    Card("Ron", "He's got a real, driving ambition", "$BASE_URL/dog4.png"),
+    Card("Sky", "The ultimate high-flyer", "$BASE_URL/dog5.png"),
+)
+
 fun Route.randomCard() {
     get("/randomcard") {
 
